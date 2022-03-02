590181830bbdbf917c2e2c91abe31dd9a7a44724
Michael B. Klein <mbklein@gmail.com>
2022-03-02 09:53:07 -0600

    Update localstack config

--- extras/localstack/terraform/main.tf
+++ extras/localstack/terraform/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   access_key                    = "fake"
   secret_key                    = "fake"
   region                        = "us-east-1"
-  s3_force_path_style           = true
+  s3_use_path_style             = true
   skip_credentials_validation   = true
   skip_metadata_api_check       = true
   skip_requesting_account_id    = true
