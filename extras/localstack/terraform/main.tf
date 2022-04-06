237d9fa113ad58fc77663ce7970f2ff0f6d73e9d
Michael B. Klein <mbklein@gmail.com>
2022-04-06 13:52:19 -0500

    Always use latest Terraform providers

--- extras/localstack/terraform/main.tf
+++ extras/localstack/terraform/main.tf
@@ -11,12 +11,11 @@ provider "aws" {
   access_key                    = "fake"
   secret_key                    = "fake"
   region                        = "us-east-1"
-  s3_force_path_style           = true
+  s3_use_path_style             = true
   skip_credentials_validation   = true
   skip_metadata_api_check       = true
   skip_requesting_account_id    = true
   
-
   endpoints {
     apigateway     = var.localstack_endpoint
     apigatewayv2   = var.localstack_endpoint
