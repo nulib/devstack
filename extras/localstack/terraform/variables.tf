8155a273709e00ea7d432fca227cecd673163638
Michael B. Klein <mbklein@gmail.com>
2022-04-29 17:45:14 -0500

    Add secrets to localstack for dev/test

---
+++ extras/localstack/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "config_secrets" {
+  type    = map(map(string))
+  default = {}
+}
+
+variable "ssl_certificate_file" {
+  type    = string
+  default = "../../miscellany/devbox_cert/dev.rdc.wildcard.full.pem"
+}
+
+variable "ssl_key_file" {
+  type    = string
+  default = "../../miscellany/devbox_cert/dev.rdc.wildcard.key.pem"
+}
+
+variable "test_mode" {
+  type    = bool
+  default = false
+}
+
+variable "user_secrets" {
+  type    = map(map(string))
+  default = {}
+}
